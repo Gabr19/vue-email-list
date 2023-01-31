aac772e2d7d6dd2da274b64d02de08aff2a10a7e
Gabr19 <sannagabriele19@gmail.com>
2023-01-31 18:08:10 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: '',
+        email:[],
       }
+    },
+    mounted(){
+        for(i = 0; i < 10; i = i + 1) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                console.log(resp.data.response);
+                this.email.push(resp.data.response)
+
+        });
     }
-  }).mount('#app')
+
+        },
+        
+  }).mount('#app');
